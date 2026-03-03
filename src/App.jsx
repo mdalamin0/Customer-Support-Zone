3ec5422f400755aec4e44da45177ee1c3398b492
Al-amin <muhammadalamin809@gmail.com>
2026-03-03 21:14:54 +0800

    project setup done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import NavBar from './Components/NavBar/NavBar'
+
+function App() {
+
+  return (
+    <div className='w-10/12 mx-auto'>
+     <NavBar></NavBar>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <div>
+      NavBar here
+    </div>
+  );
+};
+
+export default NavBar;
