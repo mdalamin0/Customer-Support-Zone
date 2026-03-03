89064fa6f8b4c4fa73337aba10f648cedddf1f66
Al-amin <muhammadalamin809@gmail.com>
2026-03-03 21:55:57 +0800

    navBar-done

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,9 +1,45 @@
-import React from 'react';
 
 const NavBar = () => {
   return (
-    <div>
-      NavBar here
+    <div className="">
+      <div className="navbar-start mt-8">
+        <div className="dropdown">
+          <div tabIndex="0" role="button" className="btn btn-ghost lg:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+          </div>
+          <ul
+            tabIndex="-1"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+            <li><a>Home</a></li>
+            <li><a>FAQ</a></li>
+            <li><a>Changelog</a></li>
+            <li><a>Blog</a></li>
+            <li><a>Download</a></li>
+            <li><a>Contact</a></li>
+          </ul>
+        </div>
+        <a className="text-sm md:text-xl font-bold">
+          CS — Ticket System
+        </a>
+      </div>
+
+      <div className="navbar-end">
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal space-x-1 font-medium ">
+            <li><a>Home</a></li>
+            <li><a>FAQ</a></li>
+            <li><a>Changelog</a></li>
+            <li><a>Blog</a></li>
+            <li><a>Download</a></li>
+            <li><a>Contact</a></li>
+          </ul>
+        </div>
+        <button className='bg-linear-to-bl from-[#632EE3] to-[#9F62F2] 
+          px-4 py-2 rounded text-sm md:text-lg font-semibold text-white cursor-pointer 
+          transition-opacity duration-300 hover:opacity-80 '>
+          + New Ticket
+        </button>
+      </div>
     </div>
   );
 };
